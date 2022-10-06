456430635555e7d0a57811adebaefc797bca7c49
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-06 11:33:28 -0300

    feat: setup initial structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// 1) Set up redux
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Lets build UBER!</Text>
     </View>
   );
 }
