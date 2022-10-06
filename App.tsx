1fba95c9918fd407b0cc20d00b07ec8e052a6c26
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-06 11:54:51 -0300

    feat: setting up and implementing redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 // 1) Set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build UBER!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets build UBER!</Text>
+      </View>
+    </Provider>
   );
 }
 
