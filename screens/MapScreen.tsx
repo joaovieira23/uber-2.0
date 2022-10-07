185bc4f6b153f2dff7bd2f14e3100156a80889ae
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-07 17:11:11 -0300

    feat: building the menu bottom

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,16 +1,27 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
     const Stack = createStackNavigator();
+    const navigation = useNavigation();
 
   return (
     <View>
+
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('HomeScreen') as never}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" /> 
+      </TouchableOpacity>
+
       <View style={tw`h-1/2`}>
           <Map />
       </View>
