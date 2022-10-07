95de25279f1ce56cff7e77d94236bd96de3dc8d3
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-07 13:16:03 -0300

    feat: building the navigate card component

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,14 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, Platform, View, KeyboardAvoidingView } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/native';
 
 const NavigateCard = () => {
+
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Boa tarde, João</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+      <View>
+            <GooglePlacesAutocomplete 
+                placeholder='Local de partida'
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                minLength={2}
+                onPress={(data, details = null) => {
+                    dispatch(setDestination({
+                        location: details?.geometry.location,
+                        desciption: data.description
+                    }));
+
+                    navigation.navigate('RideOptionsCard' as never)
+                }}
+                enablePoweredByContainer={false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: "pt-BR"
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+            />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default NavigateCard
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0
+    },
+    textInupt: {
+        backgroundColor: 'gray',
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0
+    }
+})
